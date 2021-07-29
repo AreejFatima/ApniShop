c69226e69dc097ae4e9092f9b086d47c54d92573
AreejFatima <62887754+AreejFatima@users.noreply.github.com>
2021-07-29 22:52:23 +0500

    views uploaded

---
+++ viewModels/ShoppingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ApniShop_A3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApniShop_A3.ViewModels
+{
+    public class ShoppingViewModel
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+        public string Description { get; set; }
+        virtual public string Category { get; set; }
+    }
+}
